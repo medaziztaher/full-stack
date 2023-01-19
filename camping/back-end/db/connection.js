5ddab740f8820c6eb0ade7b0f38e081904e6423a
medaziztaher <107799201+medaziztaher@users.noreply.github.com>
2023-01-19 13:51:31 +0100

    Update connection.js

--- camping/back-end/db/connection.js
+++ camping/back-end/db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const uri =
-  "mongodb+srv://medaziztaher:aziztaher1234@cluster0.ecqmfgj.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://<user>:<password>@cluster0.ecqmfgj.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(uri, (error) => {
   if (error) {
